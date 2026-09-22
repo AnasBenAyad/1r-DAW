6782967407ecc173acb118043dc6cb77265f16a9
Anas <anasbenayad338@gmail.com>
2026-09-22 15:47:30 +0200

    .git

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Demanar numero
+Console.WriteLine("Digues un numero");
+var numero = Console.ReadLine();
+
+int fnumero = Convert.ToInt32(numero);
+
+
+// Sumarli 1 al numero demanat
+
+int resultat = fnumero + 1;
+
+// Imprimir la resposta
+
+Console.WriteLine($"El numero es {resultat}");
